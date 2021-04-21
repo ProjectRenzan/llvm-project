ef23b05ae8e3606c588dbe33392d7e3501307daa
Mosakuji Hokuto <shikieiki@yamaxanadu.org>
2021-04-21 14:22:25 +0800

    revert WhitespaceManager.cpp

--- clang/lib/Format/WhitespaceManager.cpp
+++ clang/lib/Format/WhitespaceManager.cpp
@@ -698,8 +698,8 @@ void WhitespaceManager::alignConsecutiveDeclarations() {
       [](Change const &C) {
         // tok::kw_operator is necessary for aligning operator overload
         // definitions.
-        // if (C.Tok->isOneOf(TT_FunctionDeclarationName, tok::kw_operator))
-        //  return true;
+        if (C.Tok->isOneOf(TT_FunctionDeclarationName, tok::kw_operator))
+          return true;
         if (C.Tok->isNot(TT_StartOfName))
           return false;
         if (C.Tok->Previous &&
@@ -711,9 +711,8 @@ void WhitespaceManager::alignConsecutiveDeclarations() {
             continue;
           if (!Next->Tok.getIdentifierInfo())
             break;
-	  //   if (Next->isOneOf(TT_StartOfName, TT_FunctionDeclarationName,
-          //                  tok::kw_operator))
-	  if (Next->is(TT_StartOfName))
+          if (Next->isOneOf(TT_StartOfName, TT_FunctionDeclarationName,
+                            tok::kw_operator))
             return false;
         }
         return true;
